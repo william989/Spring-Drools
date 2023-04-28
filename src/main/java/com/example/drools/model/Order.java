bca6f05afe59c8c76b13c9bf7914514ee7513c5b
william989 <hwilliam.cy@gmail.com>
2023-04-28 13:16:25 -0500

    first commit

---
+++ src/main/java/com/example/drools/model/Order.java
@@ -0,0 +1,15 @@
+package com.example.drools.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Order {
+	
+	private String name;
+	private String cardType;
+	private int discount;
+	private int price;
+
+}
